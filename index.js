83b0735fc9572b5e553e6e3978200d6951f48f0f
Saman22 <samanjoodakiofficial@gmail.com>
2024-08-02 18:07:11 +0330

    Added API: Get online players using samp query

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const sampQuery = require("samp-query");
+
+const app = express();
+const PORT = 3000;
+
+const serverOptions = {
+    host: "198.186.231.197",
+    port: 7777
+}
+
+app.get("/onlinePlayers", (req, res) => {
+    sampQuery(serverOptions, (error, response) => {
+        if (error) {
+            return res.status(500).json({error: "Unable to query SAMP server"});
+        } 
+        res.json(response.players);
+    });
+})
+
+app.listen(PORT, () => {
+    console.log(Server is running on ${PORT});
+})
